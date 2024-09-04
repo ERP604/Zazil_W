bf22f5733a4f9010b601ede071baa787f07c850b
ERP604 <a01784461@tec.mx>
2024-09-04 14:34:39 -0600

    servidor

---
+++ servidor/app/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import mysql from 'mysql';
+import util from 'util';
+
+//FIX para el __dirname
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+app.set ('port', 4000);
+
+app.listen(app.get('port'), '127.0.0.1', () => {
+    console.log('Server on IP 127.0.0.1, port:', app.get('port'));
+});
+
+//Configuracion
+app.use(express.static(__dirname + "/../../public"));
+// Middleware para servir archivos .br
+app.get('*.br', function (req, res, next) {
+    res.set('Content-Encoding', 'br');
+    next();
+});
+
+app.use (express.json());
